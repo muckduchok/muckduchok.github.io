e3c966b7b58066d1bca90e6d4b9c77431d7fc830
Avhutskiy <avhtuskiy@cleverstaff.net>
2024-07-19 18:54:01 +0300

    first setting

---
+++ src/app.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div className='app'>
+      <div className='app__header'>
+        Your account
+      </div>
+
+      <div className='app__body'>
+        Maximus
+      </div>
+
+      <div className='app__footer'>
+        
+      </div>
+    </div>
+  );
+};
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from "react-dom/client";
+import { AppearanceProvider } from "@twa-dev/mark42";
+import { App } from "./app";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(<AppearanceProvider><App /></AppearanceProvider>);
